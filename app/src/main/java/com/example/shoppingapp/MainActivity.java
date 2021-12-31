32a6c585623e256d00be891e13d057c9e8440899
Chintu <bcsf18m017@pucit.edu.pk>
2021-12-31 15:04:30 +0500

    Register Activity Design

--- app/src/main/java/com/example/shoppingapp/MainActivity.java
+++ app/src/main/java/com/example/shoppingapp/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.shoppingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button loginButton,signupButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton=(Button)findViewById(R.id.main_login_button);
+        signupButton=(Button)findViewById(R.id.main_signup_button);
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,loginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,signupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/shoppingapp/signupActivity.java
+++ app/src/main/java/com/example/shoppingapp/signupActivity.java
@@ -3,12 +3,23 @@ package com.example.shoppingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class signupActivity extends AppCompatActivity {
 
+    Button signUp;
+    EditText name,phone,password,confirmPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        signUp=findViewById(R.id.signUp);
+        name=findViewById(R.id.name);
+        phone=findViewById(R.id.reg_phoneNumber);
+        password=findViewById(R.id.reg_password);
+        confirmPassword=findViewById(R.id.reg_confirm_password);
+
     }
 }
