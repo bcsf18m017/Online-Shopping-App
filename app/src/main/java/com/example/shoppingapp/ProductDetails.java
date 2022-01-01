4c8001897971722d69af20aa5f6df0137a2fd392
Chintu <bcsf18m017@pucit.edu.pk>
2022-01-01 23:23:28 +0500

    Product Details Activity Cart and Components Initialized

--- app/src/main/java/com/example/shoppingapp/ProductDetails.java
+++ app/src/main/java/com/example/shoppingapp/ProductDetails.java
@@ -3,12 +3,30 @@ package com.example.shoppingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ProductDetails extends AppCompatActivity {
 
+    FloatingActionButton cart;
+    ImageView productImage;
+    ElegantNumberButton counter;
+    TextView name,price,description;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_details);
+
+        counter=findViewById(R.id.counter);
+        name=findViewById(R.id.product_detail_name);
+        productImage=findViewById(R.id.product_detail_image);
+        description=findViewById(R.id.product_detail_description);
+        price=findViewById(R.id.product_detail_price);
+        cart=findViewById(R.id.cart_button);
+
+
     }
 }
