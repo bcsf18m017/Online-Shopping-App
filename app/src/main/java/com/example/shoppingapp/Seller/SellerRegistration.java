3098055b31ae596b9cbe4995d4c3d24de8d5c41e
Chintu <bcsf18m017@pucit.edu.pk>
2022-01-04 18:22:49 +0500

    Seller Registration Form Design

---
+++ app/src/main/java/com/example/shoppingapp/Seller/SellerRegistration.java
@@ -0,0 +1,16 @@
+package com.example.shoppingapp.Seller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.shoppingapp.R;
+
+public class SellerRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seller_registration);
+    }
+}
